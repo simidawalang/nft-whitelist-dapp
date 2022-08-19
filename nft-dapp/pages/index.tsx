22cc559101a97e52d3499c39b646ccde6016333d
simidawalang <simidawalang@gmail.com>
2022-08-19 15:35:38 +0100

    added accounts change function

--- nft-dapp/pages/index.tsx
+++ nft-dapp/pages/index.tsx
@@ -1,28 +1,54 @@
 import type { NextPage } from "next";
 import { providers } from "ethers";
 import Head from "next/head";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Button, Carousel } from "../components";
 
 const Home: NextPage = () => {
-  const getSignerOrPorvider = (needSigner: boolean = false) => {
+  const [defaultAccount, setDefaultAccount] = useState("");
+
+  const getSignerOrProvider = (needSigner: boolean = false) => {
     const provider = new providers.JsonRpcProvider();
 
     needSigner ? provider.getSigner() : provider;
   };
 
+  const checkIfConnected = async () => {
+    try {
+      const provider = new providers.Web3Provider(window.ethereum);
+
+      const accounts = await provider.send("eth_requestAccounts", []);
+
+      setDefaultAccount(accounts[0]);
+    } catch (e: any) {
+      console.error(e.message);
+    }
+  };
+
   const connectWallet = async () => {
-    
-  }
+    try {
+      const provider = new providers.Web3Provider(window.ethereum);
+
+      const accounts = await provider.send("eth_requestAccounts", []);
+
+      setDefaultAccount(accounts[0]);
+    } catch (e: any) {
+      console.error(e.message);
+    }
+  };
+
 
   useEffect(() => {
-    const checkIfConnected = async () => {
-      const { ethereum } = await window;
-
-      console.log(ethereum);
-    };
     checkIfConnected();
   }, []);
+
+  useEffect(() => {
+    window?.ethereum.on("accountsChanged", connectWallet);
+
+    return () => {
+      window?.ethereum?.removeListener("accountsChanged", connectWallet);
+    }
+  }, [defaultAccount])
 
   return (
     <div>
@@ -34,12 +60,12 @@ const Home: NextPage = () => {
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
+      <p> {defaultAccount}</p>
       <main className="homepage homepage-header container">
         <div className="intro">
           <h1 className="brand-text">ORISA</h1>
           <p>NFT collection for fans of Yoruba mythology</p>
-          <Button content="Join Whitelist" />
+          <Button content="Join Whitelist" onClick={connectWallet} />
         </div>
         <div>
           <Carousel />
