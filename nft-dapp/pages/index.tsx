58f2d48458a6f266d422df8c0104f69f29d3a810
simidawalang <simidawalang@gmail.com>
2022-08-19 14:57:32 +0100

    added declaration fike for window.ethereum, web3 connecting functions on home page

--- nft-dapp/pages/index.tsx
+++ nft-dapp/pages/index.tsx
@@ -1,29 +1,52 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import { Button, Carousel } from '../components'
+import type { NextPage } from "next";
+import { providers } from "ethers";
+import Head from "next/head";
+import { useEffect } from "react";
+import { Button, Carousel } from "../components";
 
 const Home: NextPage = () => {
+  const getSignerOrPorvider = (needSigner: boolean = false) => {
+    const provider = new providers.JsonRpcProvider();
+
+    needSigner ? provider.getSigner() : provider;
+  };
+
+  const connectWallet = async () => {
+    
+  }
+
+  useEffect(() => {
+    const checkIfConnected = async () => {
+      const { ethereum } = await window;
+
+      console.log(ethereum);
+    };
+    checkIfConnected();
+  }, []);
+
   return (
     <div>
       <Head>
         <title>ORISA</title>
-        <meta name="description" content="NFT collection for fans of Yoruba mythology" />
+        <meta
+          name="description"
+          content="NFT collection for fans of Yoruba mythology"
+        />
         <link rel="icon" href="/favicon.ico" />
-        
       </Head>
 
       <main className="homepage homepage-header container">
         <div className="intro">
           <h1 className="brand-text">ORISA</h1>
           <p>NFT collection for fans of Yoruba mythology</p>
-          <Button content="Join Whitelist"/>
+          <Button content="Join Whitelist" />
         </div>
         <div>
           <Carousel />
         </div>
       </main>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
