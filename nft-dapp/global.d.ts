08f2252e13614373c9e74655d75babb37d0edc22
simidawalang <simidawalang@gmail.com>
2022-08-20 12:39:32 +0100

    created extended type for ExternalProvider

--- nft-dapp/global.d.ts
+++ nft-dapp/global.d.ts
@@ -1,6 +1,15 @@
+import { providers } from "ethers";
+
+type ExtensionForProvider = {
+  on: (event: string, callback: (...params?: any) => void) => void;
+  removeListener:  (event: string, callback: (...params?: any) => void) => void;
+};
+
+type EthersProvider = providers.ExternalProvider & ExtensionForProvider;
+
 declare global {
   interface Window {
-    ethereum: import("ethers").providers.ExternalProvider;
+    ethereum: EthersProvider;
   }
 }
 
